2050ca123bb535b56db45974fd2ae2693db14a40
joaqimcl <joaquin.alcainob@gmail.com>
2025-12-01 18:27:49 -0300

    Arreglar redirección 404 en inicio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+// app/page.tsx
 import { redirect } from 'next/navigation'
 
 export default function Home() {
+  // En cuanto alguien entre a la raíz, lo mandamos al login
   redirect('/login')
 }
